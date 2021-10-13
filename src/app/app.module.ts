7b680f6cf53d81002ebb362550aaf4816aa68080
lguillaud <loic.guillaud@lustrel.com>
2021-10-13 14:29:29 +0200

    Start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ChampComponent } from './entites/champ/champ.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChampComponent
   ],
   imports: [
     BrowserModule
